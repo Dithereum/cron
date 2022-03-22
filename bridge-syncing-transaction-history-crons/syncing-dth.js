f1fe5b6d55f51906f343011ccb0120a2bc25ad5b
ninjavalley <92907149+ninjavalley@users.noreply.github.com>
2022-03-22 13:29:51 +0530

    syncing-dth.js   -testnet

--- bridge-syncing-transaction-history-crons/syncing-dth.js
+++ bridge-syncing-transaction-history-crons/syncing-dth.js
@@ -8,7 +8,7 @@ const Web3 = require("web3");
 var DB_CONFIG = {
 		host: "localhost",
 		user: "root",
-		password: "",
+		password: "Admin@1234",
 		database: "test",
 		connectTimeout: 100000,
 		port: 3306
@@ -31,20 +31,19 @@ const options = {
 	},
 };
 
-var DTH_CHAIN_ID = 24;	// chain ID 24 is for the Dithereum Mainnet
-var ETH_TOKEN_ADDRESS = "0x232A1fD8742a606238B53B7babA5fEe5835f3c97";
-var BNB_TOKEN_ADDRESS = "0x57012f5fE63a47a668b1fF9f6eF3D234A22e8C19";
-var MATIC_TOKEN_ADDRESS = "0xf2A16551D5ab32acf690548DcFaB1302224B9926";
-var HT_TOKEN_ADDRESS = "0x5277346c4534028C535A7e8660c491DEB63A2155";
-var DUSD_TOKEN_ADDRESS = "0xaE0e20478A312FAE7c9Fef6D80811C26b1Da0321";
+var DTH_CHAIN_ID = 34;	// chain ID 34 is for the Dithereum TESTNET
+var ETH_TOKEN_ADDRESS = "0xc1A016C2c1aCa2F381dD14d3F0a5cDbe4c986F45";
+var BNB_TOKEN_ADDRESS = "0x3D5d1a4E519335A692388A26DCc78E4dec991dFA";
+var MATIC_TOKEN_ADDRESS = "0xaF5945CdA54707E081eC52a79E5cbC19FAA42B57";
+var HT_TOKEN_ADDRESS = "0x0e7492f5D0A35A3776e6e575A195480147Bf09eA";
+var DUSD_TOKEN_ADDRESS = "0x4CA21032F6C89f9ec65F517e6dcce226714992D4";
 
 
-
-var CONTRACT_ADDR = '0x87bA9F94DB64C6a5d0221f73721Bb92008835E66';
+var CONTRACT_ADDR = '0x14B55b5Bfa8D442760Fd3e31678F38eF61cDab87';
 var CONTRACT_ADDR_ABI = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"orderID","type":"uint256"},{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"CoinIn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"orderID","type":"uint256"},{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"CoinOut","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"orderID","type":"uint256"},{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"CoinOutFailed","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_from","type":"address"},{"indexed":true,"internalType":"address","name":"_to","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"signer","type":"address"},{"indexed":true,"internalType":"bool","name":"status","type":"bool"}],"name":"SignerUpdated","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"orderID","type":"uint256"},{"indexed":true,"internalType":"address","name":"tokenAddress","type":"address"},{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"chainID","type":"uint256"}],"name":"TokenIn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"orderID","type":"uint256"},{"indexed":true,"internalType":"address","name":"tokenAddress","type":"address"},{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"chainID","type":"uint256"}],"name":"TokenOut","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"orderID","type":"uint256"},{"indexed":true,"internalType":"address","name":"tokenAddress","type":"address"},{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"chainID","type":"uint256"}],"name":"TokenOutFailed","type":"event"},{"inputs":[],"name":"acceptOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_signer","type":"address"},{"internalType":"bool","name":"_status","type":"bool"}],"name":"changeSigner","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"coinIn","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"_orderID","type":"uint256"}],"name":"coinOut","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"orderID","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"signer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"tokenAddress","type":"address"},{"internalType":"uint256","name":"tokenAmount","type":"uint256"},{"internalType":"uint256","name":"chainID","type":"uint256"}],"name":"tokenIn","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"tokenAddress","type":"address"},{"internalType":"address","name":"user","type":"address"},{"internalType":"uint256","name":"tokenAmount","type":"uint256"},{"internalType":"uint256","name":"_orderID","type":"uint256"},{"internalType":"uint256","name":"chainID","type":"uint256"}],"name":"tokenOut","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}];
 
 
-var httpprovider = new Web3(new Web3.providers.HttpProvider("https://node-mainnet.dithereum.org", options));
+var httpprovider = new Web3(new Web3.providers.HttpProvider('https://node-testnet.dithereum.io', options));
 
 let web3 = new Web3(httpprovider);
 const myinstance = new web3.eth.Contract(CONTRACT_ADDR_ABI, CONTRACT_ADDR.toString());
@@ -57,21 +56,16 @@ execute();
 
 async function execute(){
 	
-	
-	
 		var currentBlock = await web3.eth.getBlockNumber();
-		currentBlock = currentBlock-5;	//we will go 5 blocks in the past, just for safe side
-		
+		currentBlock = currentBlock-5;	//we will go 5 blocks in the past, just for safe side	   	
 
 
 		try{
-			var select_wallet_query = "SELECT * FROM lastblock";	
+			var select_wallet_query = "SELECT dithereum FROM lastblock";	
 			var lastBlockData = await query5(select_wallet_query).catch(console.log);
 			
-		
+		   		   
 			if(lastBlockData[0]){
-				
-				console.log(lastBlockData[0].dithereum);
 				var lastBlockDatabase = lastBlockData[0].dithereum;
 				
 				//updating the current block in the database
@@ -95,7 +89,6 @@ async function execute(){
 
 
 async function getEventData_TokenIn(_fromBlock, _toBlock){
-	 
 	 try{				
 		 		await myinstance.getPastEvents('TokenIn',  {
 		 				fromBlock: _fromBlock,       
@@ -108,7 +101,7 @@ async function getEventData_TokenIn(_fromBlock, _toBlock){
 		 				
 		 				for(var i=0;i<eventlen; i++){		 						 										
 		 					var eve = events[i];
-							console.log(eve)
+							console.log(eve.blockNumber);
 		 					var _blkNumber = eve.blockNumber;
 							var _txnHash = eve.transactionHash;
 		 					var _orderid = eve.returnValues.orderID;							
@@ -118,31 +111,33 @@ async function getEventData_TokenIn(_fromBlock, _toBlock){
 							var _fromCurrency = "";
 							var _toCurrency = "";
 							var _toChain = 0;
+							var _toTxnHash = '';
+							var _toAmount = 0.0;
 							
 							if(_tokenAddress==ETH_TOKEN_ADDRESS){
 								_fromCurrency = "ETH";
 								_toCurrency = _fromCurrency;
-								_toChain = 1;
+								_toChain = 4;  // RINKEBY
 							}
 							else if(_tokenAddress==BNB_TOKEN_ADDRESS){
 								_fromCurrency = "BNB";
 								_toCurrency = _fromCurrency;
-								_toChain = 56;
+								_toChain = 97;   // BINANCE TESTNET
 							}
 							else if(_tokenAddress==MATIC_TOKEN_ADDRESS){
 								_fromCurrency = "MATIC";
 								_toCurrency = _fromCurrency;
-								_toChain = 137;
+								_toChain = 137;  // MATIC MAINNET
 							}
 							else if(_tokenAddress==HT_TOKEN_ADDRESS){
 								_fromCurrency = "HT";
 								_toCurrency = _fromCurrency;
-								_toChain = 128;
+								_toChain = 256;  // HT TESTNET
 							}
 							else if(_tokenAddress==DUSD_TOKEN_ADDRESS){
 								_fromCurrency = "DUSD";
 								_toCurrency = "BUSD";
-								_toChain = 56;
+								_toChain = 97;  // BINANCE TESTNET
 							}
 							else {	//this is very unlikely to happen
 								_fromCurrency = "NA";
@@ -154,7 +149,8 @@ async function getEventData_TokenIn(_fromBlock, _toBlock){
 							if(parseInt(_amount)){							
 								try{
 									
-									var insert_query = "INSERT INTO bridge_transactions (`userWallet`,`orderID`,`fromChain`,`fromCurrency`,`fromTxnHash`,`fromAmount`,`toChain`,`toCurrency`) VALUES ('"+_userWallet+"',"+_orderid+","+DTH_CHAIN_ID+",'"+_fromCurrency+"','"+_txnHash+"',"+_amount+","+_toChain+",'"+_toCurrency+"')";		
+									var insert_query = "INSERT INTO bridge_transactions (`userWallet`,`orderID`,`fromChain`,`fromCurrency`,`fromTxnHash`,`fromAmount`,`toChain`,`toCurrency`,`toTxnHash`,`toAmount`) VALUES ('"+_userWallet+"',"+_orderid+","+DTH_CHAIN_ID+",'"+_fromCurrency+"','"+_txnHash+"',"+_amount+","+_toChain+",'"+_toCurrency+"','"+_toTxnHash+"','"+_toAmount+"')";		
+								   console.log("Insert Query >>>>", insert_query);									
 									console.log(">>> Inserting record, orderid, transactionHash >>>",_orderid, _txnHash);
 									await db_query(insert_query).catch(console.log);
 										
@@ -176,7 +172,6 @@ async function getEventData_TokenIn(_fromBlock, _toBlock){
 
 
 async function getEventData_TokenOut(_fromBlock, _toBlock){ 
-	 
 	 try{				
 		 		await myinstance.getPastEvents('TokenOut',  {
 		 				fromBlock: _fromBlock,       
@@ -189,7 +184,7 @@ async function getEventData_TokenOut(_fromBlock, _toBlock){
 		 				
 		 				for(var i=0;i<eventlen; i++){		 						 										
 		 					var eve = events[i];
-							
+		 																			
 							var _txnHash = eve.transactionHash;
 		 					var _orderid = eve.returnValues.orderID;							
 							var _userWallet = eve.returnValues.user;
@@ -200,23 +195,22 @@ async function getEventData_TokenOut(_fromBlock, _toBlock){
 														
 							if(parseInt(_amount)){							
 								try{
+									var con3 = mysql.createConnection(DB_CONFIG);
+								   const query3 = util.promisify(con3.query).bind(con3);
+									var select_from_txn_query = "SELECT * FROM bridge_transactions where fromChain='"+_chainID+"' && orderID='"+_orderid+"' && status='Pending'";										
+									var fromTxnData = await query3(select_from_txn_query).catch(console.log);
 									
-									var select_from_txn_query = "SELECT * FROM bridge_transactions where fromChain='"+_chainID+"' && orderID='"+_orderid+"' && status='Pending'";	
-									var fromTxnData = await query5(select_from_txn_query).catch(console.log);
-									
-									
+																	
 									//take the first result. Even any duplicate by mistake, then also it will take first one.
-									if(fromTxnData[0]){	
+									if(fromTxnData[0]){
 										var update_query = "UPDATE `bridge_transactions` SET `toTxnHash`='"+_txnHash+"',`toAmount`='"+_amount+"',`status`='Completed' WHERE `fromChain`='"+_chainID+"' && `orderID`='"+_orderid+"'";		
 										console.log(">>> Updating record, orderid, transactionHash >>>",_orderid, _txnHash);
 										await db_query(update_query).catch(console.log);
 									}
 									else{
 										//just in case, the order ID and chain ID are not found in the db, then it will log it. This is very rare to happen.
-										console.log("Unprocessed Transaction: chain, Hash: "+DTH_CHAIN_ID+" - "+_txnHash);
-									}
-									
-										
+										console.log("--Unprocessed Transaction: chain, Hash: "+DTH_CHAIN_ID+" - "+_txnHash);
+									}	
 								}catch(e){
 									console.log(">>>>>Catch >>>>",e);									
 								}																
